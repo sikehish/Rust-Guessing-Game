b2761dd405f661689052dd86efd49108a7f364ed
sikehish <hisham0502@gmail.com>
2025-03-14 00:03:00 +0530

    Added random num generator logic

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+use rand::Rng;
 use std::io;
-
 fn main() {
-    println!("Guess the number");
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
     println!("Please input your guess.");
-    let mut guess=String::new();
-
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
-    println!("You guessed: {}", guess);
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("You guessed: {guess}");
 }
